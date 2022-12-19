54111c4d9eae145d8f932c0cf01cfcd424ac019e
Harsh Shukla <harshshukla2807@gmail.com>
2022-12-20 00:20:54 +0530

    Delete workout button created

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -33,27 +33,27 @@ const createWorkout = async (req,res)=>{
     }catch(error){
         res.status(400).json({error: error.message})
     }
+    
 }
 
 
 // delete a workout 
 const deleteWorkout=async(req,res)=>{
-    const id =req.params.id
-    
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "no such workout"})
-    }
-    
-    const workout= await Workout.findByIdAndDelete({_id:id})
-    
-    if(!workout){
-        return  res.status(404).json({error: "no such workout"})
-    }
-    
-    res.status(404).json(workout)
-    
-   Workout.remove(req.params.id)
-   res.status(200).json(Workout)
+    const { id } = req.params
+    // console.log(id)
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndDelete({_id: id})
+
+  if(!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+ return res.status(202).json(workout)
+  
+  
 }
 
 // update a workout 
--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,23 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ({ workout }) => {
+  
+  const {dispatch}=useWorkoutsContext()
+  
+  const handleDelete=async ()=>{
+ 
+      const response= await fetch('/api/workouts/'+ workout._id ,{
+        method: 'POST'
+      })
+      
+      const json = await response.json()
+      
+      if(response.ok){
+        dispatch({type: 'DELETE_WORKOUT',payload:json})
+      }
+    } 
+    // console.log(response)
+  
 
   return (
     <div className="workout-details">
@@ -6,6 +25,7 @@ const WorkoutDetails = ({ workout }) => {
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleDelete}>Delete</span>
     </div>
   )
 }
